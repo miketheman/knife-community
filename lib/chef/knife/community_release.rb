6e0b875b08d257b6d41276c58b02b65bdb31f85e
Mike Fiedler <miketheman@gmail.com>
2012-09-17 02:59:23 -0400

    Late night, completed most functionality.
    - Needs a fair amount of testing. Need help with RSpec mocking of cookbooks, repositories
    - All main functionality from README steps has been implemented. RDD, indeed!

--- lib/chef/knife/community_release.rb
+++ lib/chef/knife/community_release.rb
@@ -6,9 +6,11 @@ module KnifeCommunity
     deps do
       require 'knife-community/version'
       require 'mixlib/shellout'
+      require 'chef/config'
       require 'chef/cookbook_loader'
       require 'grit'
       require 'versionomy'
+      require 'json'
     end
 
     banner "knife community release COOKBOOK [VERSION] (options)"
@@ -37,18 +39,34 @@ module KnifeCommunity
       :description => "Odd-numbered development cycle. Bump minor version & commit for development"
 
     def run
+      self.config = Chef::Config.merge!(config)
       validate_args
       # Set variables for global use
       @cookbook = name_args.first
       @version = Versionomy.parse(name_args.last) if name_args.size > 1
 
-      # Do a bunch of validations before we change anything
+      ui.msg "Starting to validate the envrionment before changing anything..."
       validate_cookbook_exists
       validate_repo
       validate_repo_clean
       validate_version_sanity
+      validate_no_existing_tag
+      validate_target_remote_branch
 
+      ui.msg "All validation steps have passed, making changes..."
+      set_new_cb_version
+      commit_new_cb_version
+      tag_new_cb_version
+      git_push_commits
+      git_push_tags
 
+      share_new_version
+
+      ui.msg "Version #{@version} of the #{@cb_name} has been released!"
+      ui.msg "Check it out at http://ckbk.it/#{@cb_name}"
+
+      # @TODO: Increment the current version to the next available odd number
+      # algo = n + 1 + (n % 2)
       if config[:devodd]
         puts "I'm odd!"
       end
@@ -96,7 +114,7 @@ module KnifeCommunity
     end
 
     # Ensure that the cookbook is in a git repo
-    # @todo OPTIMIZE: Use Grit instead of shelling out.
+    # @TODO: Use Grit instead of shelling out.
     # Couldn't figure out the rev_parse method invocation on a non-repo.
     #
     # @return [String] The absolute file path of the git repository's root
@@ -104,10 +122,7 @@ module KnifeCommunity
     #  "/Users/miketheman/git/knife-community"
     def validate_repo
       begin
-        proc = Mixlib::ShellOut.new("cd #{@cb_path} && git rev-parse --show-toplevel")
-        proc.run_command
-        proc.error!
-        @repo_root = proc.stdout.chomp
+        @repo_root = shellout("git rev-parse --show-toplevel").stdout.chomp
       rescue Exception => e
         ui.error "There doesn't seem to be a git repo at #{@cb_path}\n#{e}"
         exit 3
@@ -117,10 +132,11 @@ module KnifeCommunity
     # Inspect the cookbook directory's git status is good to push.
     # Any existing tracked files should be staged, otherwise error & exit.
     # Untracked files are warned about, but will allow continue.
+    # This needs more testing.
     def validate_repo_clean
       @gitrepo = Grit::Repo.new(@repo_root)
       status = @gitrepo.status
-      if !status.changed.nil? or status.changed != 0 # This has to be a convoluted way to determine a non-empty...
+      if !status.changed.nil? or status.changed.size != 0 # This has to be a convoluted way to determine a non-empty...
         # Test each for the magic sha_index. Ref: https://github.com/mojombo/grit/issues/142
         status.changed.each do |file|
           case file[1].sha_index
@@ -131,7 +147,7 @@ module KnifeCommunity
             ui.msg "There are modified files that have been staged, and will be included in the push."
           end
         end
-      elsif status.untracked > 0
+      elsif status.untracked.size > 0
         ui.warn "There are untracked files in your repo. You might want to look into that."
       end
     end
@@ -148,6 +164,75 @@ module KnifeCommunity
         ui.error "For your own sanity, don't release historical cookbooks into the wild."
         exit 5
       end
+    end
+
+    # Ensure that there isn't already a git tag for this version.
+    def validate_no_existing_tag
+      existing_tags = Array.new
+      @gitrepo.tags.each { |tag| existing_tags << tag.name }
+      if existing_tags.include?(@version.to_s)
+        ui.error "This version tag has already been committed to the repo."
+        ui.error "Are you sure you haven't released this already?"
+        exit 6
+      end
+    end
+
+    # Ensure that the remote and branch are indeed valid. We provide defaults in options.
+    def validate_target_remote_branch
+      remote_path = File.join(config[:remote], config[:branch])
+
+      remotes = Array.new
+      @gitrepo.remotes.each { |remote| remotes << remote.name }
+      unless remotes.include?(remote_path)
+        ui.error "The remote/branch specified does not seem to exist."
+        exit 7
+      end
+    end
+
+    # Replace the existing version string with the new version
+    def set_new_cb_version
+      metadata_file = File.join(@cb_path, "metadata.rb")
+      fi = File.read(metadata_file)
+      fi.gsub!(@cb_version.to_s, @version.to_s)
+      fo = File.open(metadata_file, 'w') { |file| file.puts fi }
+    end
+
+    # Using shellout as needed.
+    # @todo Struggled with the Grit::Repo#add for hours.
+    def commit_new_cb_version
+      shellout("git add metadata.rb")
+      @gitrepo.commit_index("release v#{@version}")
+    end
+
+    def tag_new_cb_version
+      shellout("git tag -a -m 'release v#{@version}' #{@version}")
+    end
+
+    # Apparently Grit does not have any `push` semantics yet.
+    def git_push_commits
+      shellout("git push #{config[:remote]} #{config[:branch]}")
+    end
+
+    def git_push_tags
+      shellout("git push #{config[:remote]} --tags")
+    end
+
+    def share_new_version
+      # Need to find the existing cookbook's category. Thankfully, this is readily available via REST/JSON.
+      response = Net::HTTP.get_response("cookbooks.opscode.com", "/api/v1/cookbooks/#{@cb_name}")
+      category = JSON.parse(response.body)['category'] ||= "Other"
+
+      # The use the existing knife command to tarball & share.
+      # This could possibly done in a more elegant fashion than shelling out again.
+      knife_ckbk_path = "-o #{config[:cookbook_path].join(':')}" if !config[:cookbook_path].nil?
+      shellout("knife cookbook site share #{@cb_name} #{category} #{knife_ckbk_path}")
+    end
+
+    def shellout(command)
+      proc = Mixlib::ShellOut.new(command, :cwd => @cb_path)
+      proc.run_command
+      proc.error!
+      return proc
     end
 
   end #class
